24316cf45f176e1aa499d5e56b5e883468fc2f9b
BeeVe <erick129ferreira@gmail.com>
2025-04-09 17:42:59 -0300

    update if else

--- IF_ELSE.js
+++ IF_ELSE.js
@@ -1,40 +1,72 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>escreva</title>
 	<style>
+		body {
+			/*background-color: #e6b967;*/
+			background-color: #d99611;
+			
+		} 
+
+        h1{
+			color: #e08422;
+			margin: 20px;
+			background-color: #e6b967 ;
+		} 
+
 		p {
-    font-size: 2rem;
-    color: blue;
-    margin: 20px;
-}
+			font-size: 2rem;
+			color: #e08422;
+			margin: 20px;
+			background-color: #e6b967 ;
+
+		}
+
+		#caixa {
+			/*espaço interno*/
+			padding-right: 2rem;
+			padding-bottom: 2rem;
+			margin: 2rem;
+			background-color: #e6b967;
+			border-radius: 5px;
+			color: #e82;
+			font-size: medium;
+			font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+		}
+
 	</style>
 </head>
+
 <body>
 	<h1>Boas Vindas!</h1>
-<script lang="javascript">
-	<!--
-	var pessoa = {nome:"",sobrenome:"", idade:""};
-	pessoa.nome = window.prompt('escreva seu nome', 'fulano');
-	pessoa.sobrenome = window.prompt('escreva seu nome', 'da silva');
-	pessoa.idade = Number(window.prompt('digite sua idade', 5));
-   if(pessoa.nome == null && pessoa.sobrenome == null || pessoa.nome == 'fulano' && pessoa.sobrenome == "da silva"){
-   pessoa.nome = "sem nome,";
-   pessoa.sobrenome = "sem sobrenome";
-   } else{
-	document.write("<p>Olá "+ pessoa.nome +" "+pessoa.sobrenome+", que tem "+pessoa.idade+ " anos, como você está?</p>");
-   }
-   
-   if(isNaN(pessoa.idade) || pessoa.idade <= 0){
 
-	document.write("<p>Olá "+ pessoa.nome +" "+pessoa.sobrenome+" e idade inválida ("+pessoa.idade+"), como você está?</p>");
-   } else{
-	document.write("<p>Olá "+ pessoa.nome +" "+pessoa.sobrenome+", que tem "+pessoa.idade+ " anos, como você está?</p>");
-   }
-	//próxima parada [Math.floor(Math.random() * nomes.length)];
-	//-->
-</script>
+	<script lang="javascript">
+		<!--
+		var pessoa = {nome:"",sobrenome:"", idade:""};
+		pessoa.nome = window.prompt('escreva seu nome', 'fulano');
+		pessoa.sobrenome = window.prompt('escreva seu nome', 'da silva');
+		pessoa.idade = Number(window.prompt('digite sua idade', 5));
+	   if(pessoa.nome == null && pessoa.sobrenome == null || pessoa.nome == 'fulano' && pessoa.sobrenome == "da silva"){
+	   pessoa.nome = "sem nome,";
+	   pessoa.sobrenome = "sem sobrenome";
+	   } else{
+		document.write("<p>Olá "+ pessoa.nome +" "+pessoa.sobrenome+", que tem "+pessoa.idade+ " anos, como você está?</p>");
+	   }
+	   
+	   if(isNaN(pessoa.idade) || pessoa.idade <= 0){
+	
+		document.write("<p>Olá "+ pessoa.nome +" "+pessoa.sobrenome+" e idade inválida ("+pessoa.idade+"), como você está?</p>");
+	   } else{
+		document.write("<p>Olá "+ pessoa.nome +" "+pessoa.sobrenome+", que tem "+pessoa.idade+ " anos, como você está?</p>");
+	   }
+		//próxima parada [Math.floor(Math.random() * nomes.length)];
+		//-->
+	</script>
+		<textarea id="caixa">Escreva alguma coisa</textarea>
 
 </body>
+
 </html>
